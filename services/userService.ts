ee328de21b1735b83e398956a867697ea635621d
Fernando Neto <fernando39357@gmail.com>
2024-07-10 19:23:00 -0300

    feat: saving user on database

--- services/userService.ts
+++ services/userService.ts
@@ -1,13 +1,20 @@
 "use server";
 
+import { db } from "@/lib/db";
+import { v7 } from "uuid";
 import { RegisterSchema, TRegisterProps } from "@/lib/schemas/registerSchema";
 import { TResult } from "@/types/result";
+import bcrypt from "bcryptjs";
+
+type TRegisterResult = {
+  id: string;
+};
 
 export async function register({
   name,
   email,
   password,
-}: TRegisterProps): Promise<TResult> {
+}: TRegisterProps): Promise<TResult<TRegisterResult>> {
   const parseResult = RegisterSchema.safeParse({ name, email, password });
 
   if (!parseResult.success) {
@@ -16,11 +23,23 @@ export async function register({
       error: parseResult.error.message,
     };
   }
+  const id = v7();
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await db.user.create({
+    data: {
+      id,
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
   return {
     success: true,
-    data: undefined,
+    data: {
+      id,
+    },
   };
 }
